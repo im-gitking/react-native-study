c2534b41c6aa53d1a0e66636e6ad63934bf5e27d
Abhishek Paul <avipl577@gmail.com>
2024-10-30 11:39:51 +0530

    Image Component

--- src/app/Index.tsx
+++ src/app/Index.tsx
@@ -1,12 +1,14 @@
 import { Text, View } from "react-native";
 import ViewComponent from "../components/ViewComponent";
 import TextComponent from "../components/TextComponent";
+import ImageComponent from "../components/ImageComponent";
 
 const Index = () => {
   return (
     <View>
       <TextComponent />
       <ViewComponent />
+      <ImageComponent />
     </View>
   );
 };
---
+++ src/components/ImageComponent.tsx
@@ -0,0 +1,24 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+
+const ImageComponent = () => {
+  return (
+    <View>
+      <Text>Lets Explore Images</Text>
+
+      <Image
+        source={require("../assets/lion.webp")}
+        style={{ width: 200, height: 200 }}
+      />
+
+      <Image
+        source={{
+          uri: "https://reactnative.dev/img/tiny_logo.png",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+    </View>
+  );
+};
+
+export default ImageComponent;
