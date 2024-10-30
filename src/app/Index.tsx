3fb91f6fab3bf15bb49c91130b77ab02a51e254e
Abhishek Paul <avipl577@gmail.com>
2024-10-30 13:37:21 +0530

    Project: HugeList Component

--- src/app/Index.tsx
+++ src/app/Index.tsx
@@ -1,12 +1,14 @@
 import { Text, View } from "react-native";
 import ListData from "../components/ListData";
 import ArrayOfObject from "../components/ArrayOfObject";
+import HugeComponent from "../components/HugeComponent";
 
 const Index = () => {
   return (
     <View>
       {/* <ListData /> */}
-      <ArrayOfObject />
+      {/* <ArrayOfObject /> */}
+      <HugeComponent />
     </View>
   );
 };
